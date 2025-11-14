b1e98e9149e58ffc537a9abe5871a1a71373a25a
Vamsi Namburu <vamsi.namburu@news.com.au>
2025-11-14 14:39:02 +1100

    added agent

--- agent.py
+++ agent.py
@@ -2,24 +2,19 @@ from google.adk.agents import Agent
 from google.adk.tools.agent_tool import AgentTool
 
 from .subagents.analyst_agent.agent import analyst_agent
+from .subagents.dispatcher_agent.agent import dispatcher_agent
+from .subagents.executive_agent.agent import reporter_agent
+from .prompts import return_instructions_root
+
+
 root_agent = Agent(
     name="bq-analyst",
-    model="gemini-2.0-flash",
+    model="gemini-2.5-pro",
     description="Data Science Agent",
-    instruction="""
-    You are a manager agent that is responsible for overseeing the work of the other agents.
-
-    Always delegate the task to the appropriate agent. Use your best judgement 
-    to determine which agent to delegate to.
-
-    You are responsible for delegating tasks to the following agent:
-    - stock_analyst
-    - funny_nerd
-
-    You also have access to the following tools:
-    - news_analyst
-    - get_current_time
-    """,
-    sub_agents=[analyst_agent],
-    tools=[],
+    instruction=return_instructions_root(),
+    sub_agents=[analyst_agent, dispatcher_agent, executive_agent],
+    tools=[
+        AgentTool(news_analyst),
+        get_current_time,
+    ],
 )
