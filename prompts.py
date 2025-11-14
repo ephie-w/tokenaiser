37a2979153dc7f3050387eabf636fd4c7b158b0c
ephie <ephie.wang@news.com.au>
2025-11-14 14:46:51 +1100

    feat

---
+++ prompts.py
@@ -0,0 +1,21 @@
+'''
+Author: Yifei Wang
+Github: ephiewangyf@gmail.com
+Date: 2025-11-14 14:46:29
+LastEditors: ephie && ephiewangyf@gmail.com
+LastEditTime: 2025-11-14 14:46:39
+FilePath: /tokenaiser/prompts.py
+Description: 
+'''
+def return_instructions_root() -> str:
+    return """
+    You are a manager agent that is responsible for overseeing the work of the other agents.
+
+    Always delegate the task to the appropriate agent. Use your best judgement 
+    to determine which agent to delegate to.
+
+    You are responsible for delegating tasks to the following agent:
+    - analyst_agent
+    - dispatcher_agent
+    - reporter_agent
+    """
--- subagents/analyst_agent/agent.py
+++ subagents/analyst_agent/agent.py
@@ -3,7 +3,7 @@ Author: Yifei Wang
 Github: ephiewangyf@gmail.com
 Date: 2025-11-14 14:06:39
 LastEditors: ephie && ephiewangyf@gmail.com
-LastEditTime: 2025-11-14 14:38:33
+LastEditTime: 2025-11-14 14:45:01
 FilePath: /tokenaiser/subagents/analyst_agent/agent.py
 Description: 
 '''
@@ -18,11 +18,10 @@ from google.genai import types
 from . import tools
 ADK_BUILTIN_BQ_EXECUTE_SQL_TOOL = "execute_sql"
 logger = logging.getLogger(__name__)
-analytics_agent = Agent(
+analyst_agent = Agent(
     model=os.getenv("ANALYTICS_AGENT_MODEL", ""),
     name="analytics_agent",
     instruction="You are an analyst agent responsible for analyzing data and providing insights.",
-
     tools=[
         tools.bq_get_table_info,
         tools.bq_get_dataset_info,
