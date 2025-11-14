e87d45be88aec5a7ed75f387f011a2246bf1e68f
Vamsi Namburu <vamsi.namburu@news.com.au>
2025-11-14 14:58:42 +1100

    added dispatcher agent

--- subagents/dispatcher_agent/agent.py
+++ subagents/dispatcher_agent/agent.py
@@ -1,1 +1,25 @@
-#dummy
+from google.adk.agents import Agent
+from google.adk.tools.tool_context import ToolContext
+
+from .prompts import return_instructions_dispatcher
+
+
+def dispatch_consumer(tool_context: ToolContext) -> dict:
+    """Invoke API to dispatch notification to consumer channel."""
+    
+
+    return {"status": "success", "dispatch_event": "Subscriptions have dipped by %10 against daily average"}
+
+def dispatch_editorial(tool_content: ToolContext) -> dict:
+    """Invoke API to dispatch notification to editorial channel."""
+
+    return {"status": "success", "dispatch_event": "COnversion rate has decreased by 20%"}
+
+# Create the funny nerd agent
+dispatcher_agent = Agent(
+    name="dispatcher_agent",
+    model="gemini-2.5-pro",
+    description="An agent that tells nerdy jokes about various topics.",
+    instruction=return_instructions_dispatcher(),
+    tools=[dispatch_consumer,dispatch_editorial],
+)
---
+++ subagents/dispatcher_agent/prompts.py
@@ -0,0 +1,4 @@
+def return_instructions_dispatcher() -> str:
+    return """
+    Prompt for dispatching messages through APIs
+    """
