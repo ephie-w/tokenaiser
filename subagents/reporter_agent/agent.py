56fa8ff4374cdbbcf3998bb3380c97158c98d4e2
namburuv <46011242+namburuv@users.noreply.github.com>
2025-11-14 15:48:33 +1100

    Update agent.py

--- subagents/reporter_agent/agent.py
+++ subagents/reporter_agent/agent.py
@@ -1,1 +1,15 @@
-#dummy
+from google.adk.agents import Agent
+
+from .prompts import return_instructions_reporter
+
+reporter_agent = Agent(
+    name="reporter_agent",
+    model="gemini-2.5-pro",
+    description="An agent that dispatches notifications to different channels.",
+    instruction=return_instructions_reporter(),
+    tools=[
+        tools.dispatch_consumer,
+        tools.dispatch_editorial,
+        tools.apihub
+    ],
+)#dummy
